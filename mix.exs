a46840a7d1763f70f699663cf1084c8b4e2cdc6e
Steffen Deusch <steffen@deusch.me>
2025-05-25 12:36:22 +0200

    release v0.1.7

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Tidewave.MixProject do
   def project do
     [
       app: :tidewave,
-      version: "0.1.6",
+      version: "0.1.7",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
