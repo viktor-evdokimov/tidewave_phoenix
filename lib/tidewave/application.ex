d2ccb314d523c7feac7e2a2add28e734247b25e8
José Valim <jose.valim@gmail.com>
2025-06-13 15:46:49 +0200

    Log if Tidewave is starting but Mix is not available (#98)

--- lib/tidewave/application.ex
+++ lib/tidewave/application.ex
@@ -1,12 +1,17 @@
 defmodule Tidewave.Application do
   @moduledoc false
   use Application
+  require Logger
 
   @impl true
   def start(_type, _args) do
-    children = [
-      Tidewave.MCP
-    ]
+    children =
+      if Application.spec(:mix, :vsn) do
+        [Tidewave.MCP]
+      else
+        Logger.warning("application :tidewave is not starting because Mix is not running")
+        []
+      end
 
     opts = [strategy: :one_for_one, name: Tidewave.Supervisor]
     Supervisor.start_link(children, opts)
